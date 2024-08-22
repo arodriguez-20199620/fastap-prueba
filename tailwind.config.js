4d06251cd8eeeabbbf893fc94abf52693cfa6e84
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2024-08-22 17:07:14 -0600

    Rutas

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,12 +21,12 @@ export default {
         customLightBlue: '#A6D3EA',
         customDarkGray: '#333333',
       },
-          backgroundImage: {
-        'gym': "url('./assets/images/gymsection.jpg')",
-        'cases': "url('./assets/images/casessection.jpg')",
-        'jewelry': "url('./assets/images/jewelsection.jpg')",
-        'cases-gallery': "url('./assets/images/cases.jpg')",
-    },
+      backgroundImage: {
+        'gym': "url('/assets/images/gymsection.jpg')",
+        'cases': "url('/assets/images/casessection.jpg')",
+        'jewelry': "url('/assets/images/jewelsection.jpg')",
+        'cases-gallery': "url('/assets/images/cases.jpg')",
+      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'], // Fuente Poppins para un estilo juvenil     
       },
